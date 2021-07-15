839904d8be4f88cf1140f9241694f8291c7d0746
leoxshn <leo@posidon.io>
2021-07-15 13:03:59 +0200

    improved the getNavigationBarHeight function again

--- lib/src/main/java/posidon/android/conveniencelib/Activity.kt
+++ lib/src/main/java/posidon/android/conveniencelib/Activity.kt
@@ -1,10 +1,14 @@
 package posidon.android.conveniencelib
 
 import android.app.Activity
+import android.graphics.Point
 import android.os.Build
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.WindowInsets
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.min
 
 inline fun Activity.hideKeyboard() {
     val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -16,10 +20,17 @@ inline fun Activity.hideKeyboard() {
 }
 
 inline fun Activity.getNavigationBarHeight(): Int {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         windowManager.currentWindowMetrics.windowInsets.getInsets(WindowInsets.Type.navigationBars()).bottom
     } else {
-        val id = resources.getIdentifier("navigation_bar_height", "dimen", "android")
-        if (id > 0) resources.getDimensionPixelSize(id) else 0
+        val display = display ?: return 0
+        val metrics = DisplayMetrics()
+        display.getMetrics(metrics)
+        val usableHeight = metrics.heightPixels
+        display.getRealMetrics(metrics)
+        val realHeight = metrics.heightPixels
+        val navbarHeight = if (realHeight > usableHeight) realHeight - usableHeight else 0
+        val resourceId: Int = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        min(if (resourceId > 0) resources.getDimensionPixelSize(resourceId) else 0, navbarHeight)
     }
 }
